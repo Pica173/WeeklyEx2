4f6cd8a02040adefbea65533279b71e34b9795fd
“Pica173” <otasanlife2020@gmail.com>
2022-07-09 01:19:23 +0900

    first-commit

--- Problem2/RoomController.php
+++ Problem2/RoomController.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
-    public function index($text = "建物です")
+    public function room($room = "建物です")
     {
-        return $text;
-    }
-        public function room($room = "建物です")
-    {
-        return "部屋番号は" .$room . "です";
+        if($room <> "建物です"){
+            return "部屋番号は" .$room . "です";
+        } else {
+            return $room;
+        }
+        
     }
 }
--- Problem2/web.php
+++ Problem2/web.php
@@ -3,7 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
 
-Route::prefix('building')->group(function () {
-    Route::get('', [RoomController::class, 'index']);
-    Route::get('/{room}', [RoomController::class, 'room']);
-});
+Route::get('/building/{room?}', [RoomController::class, 'room']);
